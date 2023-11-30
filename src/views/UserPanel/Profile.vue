c3a42d603ba0102f85b0c7523b38ba7b589db89e
RondaKuraI <johnronda0527@gmail.com>
2023-11-30 23:58:50 +0800

    Update Profile.vue

--- src/views/UserPanel/Profile.vue
+++ src/views/UserPanel/Profile.vue
@@ -9,6 +9,17 @@
             </v-col>
           </v-row>
         </v-container>
+
+        <v-container>
+            <v-card>
+                <v-form fast-fail @submit.prevent>
+                    <v-text-field v-model="firstName" label="First name" :rules="firstNameRules"></v-text-field>
+                    <v-text-field v-model="middleName" label="Middle name" :rules="middleNameRules"></v-text-field>
+                    <v-text-field v-model="lastName" label="Last name" :rules="lastNameRules"></v-text-field>
+                    <v-btn type="submit" block class="mt-2">Submit</v-btn>
+                </v-form>
+            </v-card>
+        </v-container>
     </div>
 </template>
 
